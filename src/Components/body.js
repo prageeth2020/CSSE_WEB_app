3608d551ef62a87a7a29273e023ca12aa3e2090c
prageeth2020 <prageethpramuditha.2020@gmail.com>
2020-10-16 16:25:11 +0530

    navigration done

--- src/Components/body.js
+++ src/Components/body.js
@@ -1,27 +1,33 @@
 import React, {Component} from 'react';
-
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Dashboard from "./Dashboard";
+import AllOrders from "./allOrders";
+import Pending from "./pending";
+import SignOut from "./SignOut";
+import Complains from "./Complains";
 class Body extends Component {
     render() {
         return (
-            <div>
-                <div className="row ">
-                    <div className="col-sm-2  border-right bg-light border-secondary">
+            <div className="">
+                <Router>
+                    <div className="row mx-1 height">
+                        <div className="col-sm-2  bg-light  ">
+                                <Link className="nav-link link border-bottom text-dark" to="/"> <li className="p-2  d-flex justify-content-between align-items-center">Dashboard  </li></Link>
+                                <Link className="nav-link link border-bottom text-dark" to="/orders"> <li className="p-2 d-flex justify-content-between align-items-center">Orders </li></Link>
+                                <Link className="nav-link link border-bottom text-dark" to="/Pending"> <li className="p-2  d-flex justify-content-between align-items-center">Pending Approvals  <span className="badge badge-danger badge-pill">1</span></li></Link>
+                                <Link className="nav-link link border-bottom text-dark" to="/Complains"> <li className="p-2  d-flex justify-content-between align-items-center">Complains <span className="badge badge-danger badge-pill">1</span></li></Link>
+                                <Link className="nav-link link border-bottom text-dark" to="/SignOut"> <li className="p-2  d-flex justify-content-between align-items-center">SignOut </li></Link>
+                        </div>
+                        <div className="col-sm-10 my-2 ">
+                            <Route exact path="/" component={Dashboard} />
+                            <Route exact path="/orders" component={AllOrders} />
+                            <Route exact path="/Pending" component={Pending} />
+                            <Route exact path="/Complains" component={Complains} />
+                            <Route exact path="/SignOut" component={SignOut} />
 
-                        <li className="p-3 border-bottom d-flex justify-content-between align-items-center">Dashboard  <span className="badge badge-primary badge-pill">14</span></li>
-                        <li className="p-3 border-bottom d-flex justify-content-between align-items-center">Dashboard  <span className="badge badge-primary badge-pill">14</span></li>
-                        <li className="p-3 border-bottom d-flex justify-content-between align-items-center">Dashboard  <span className="badge badge-primary badge-pill">14</span></li>
-                        <li className="p-3 border-bottom d-flex justify-content-between align-items-center">Dashboard  <span className="badge badge-primary badge-pill">14</span></li>
-                        <li className="p-3 border-bottom d-flex justify-content-between align-items-center">Dashboard  <span className="badge badge-primary badge-pill">14</span></li>
-
-                    </div>
-                    <div className="col-sm-10 my-2">
-                        <div className="card mx-2">
-                            <div className="card-body">
-
-                            </div>
                         </div>
                     </div>
-                </div>
+                </Router>
             </div>
         );
     }
---
+++ src/Components/pending.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Pending extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pending</h1>
+            </div>
+        );
+    }
+}
+
+export default Pending;
